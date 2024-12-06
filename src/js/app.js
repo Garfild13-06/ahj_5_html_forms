6fffa1311dbf2a13f705cebc7d0e381f50c23933
Aleksey <garfild13-06@yandex.com>
2024-12-06 21:38:59 +0300

    Init Commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("popoverButton");
+const popover = document.getElementById("popover");
+
+button.addEventListener("click", () => {
+  popover.classList.toggle("active");
+});
